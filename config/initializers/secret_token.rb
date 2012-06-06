53c5d1bd86d5f1141e779f9802f441c672481807
zhntr <zhntr@localhost.localdomain>
2012-06-05 20:54:45 -0500

    Initial commit rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Buscador::Application.config.secret_token = '599fcf2c7791964f0320052fb065d2af015a4a78d6b213c43e0d913044f04208106c8a06796fe587f444390a924923330f55f2992d93aa23195c8f4c83867518'
