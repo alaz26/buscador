643226ebd0e756b7e0b99d8e90030d4a2e7d9190
Juan Enrique Herrejon Guevara <xz.hunterz@gmail.com>
2012-06-06 15:32:29 -0500

    Fixed error home -> index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Buscador::Application.routes.draw do
   devise_for :users
 
-  root :to => "static_pages#home"
+  root :to => "static_pages#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
